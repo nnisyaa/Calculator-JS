0e62e9d83bde2020d3d53d8c03906f981986ef44
Nisa Hasratova <nisa@Nisas-MacBook-Air.local>
2025-09-07 18:57:19 +0400

    minus-plus and delete

--- script.js
+++ script.js
@@ -14,10 +14,47 @@ function clearAll(){
     finish = false;
     out.textContent = 0;
 }
+function deleteLast(){
+    if(sign === ''){
+        if(a!== ''){
+            a = a.slice(0, -1);
+            out.textContent = a === '' ? '0' : a;
+        }
+    }else{
+        if(b !==''){
+            b = b.slice(0, -1);
+            out.textContent = b === '' ? '0' : b;
+        } else{
+            sign = '';
+            out.textContent = a === '' ? '0' : a;
+        }
+    }
+}
+function oppositeSign() {
+    if (sign === '') {
+        if (a !== '') {
+            a = (-a).toString();
+            out.textContent = a || 0;
+        }
+    } else {
+        if (b !== '') {
+            b = (-b).toString();
+            out.textContent = b || 0;
+        }
+    }
+}
+document.querySelector('.delete').onclick = (e) =>{
+    deleteLast();
+    e.stopPropagation();
+    // console.log('hello')
+}
+document.querySelector('.minus-plus').onclick = oppositeSign;
 document.querySelector('.ac').onclick = clearAll;
 document.querySelector('.buttons').onclick = (event)=>{
     if(!event.target.classList.contains('btn')) return;
     if(event.target.classList.contains('ac')) return;
+    if(event.target.classList.contains('minus-plus')) return;
+    if(event.target.classList.contains('delete')) return;
 
     out.textContent = '';
     const key = event.target.textContent;
